dcf6d95cb3acdf1696bd2e9a88170d5a6e17dda1
Viktor Menshenin <vix@Viktors-MBP.mshome.net>
2024-02-26 17:31:46 +0500

    HW 8 - well done. Some added to README

---
+++ hw8/hw_8.py
@@ -0,0 +1,292 @@
+import os
+import json
+phone_book = []
+phone_book_file = "phonebook.json"
+
+def show_menu():
+    """
+    Функция отображает меню и возвращает выбор пользователя
+
+    Returns:
+        coise (int): Возвращает пользовательский ответ 
+    """
+    print("\nВыберите необходимое действие:\n",
+          "1. Отобразить весь справочник\n",
+          "2. Найти абонента\n",
+          "3. Добавить абонента в справочник\n",
+          "4. Изменить абонента\n",
+          "5. Удалить абонента\n",
+          "6. Сохранить справочник в текстовом формате\n",
+          "7. Закончить работу"),
+    choice = int(input())
+    return choice
+
+
+def show_search_menu():
+    """
+    Функция отображает меню поиска
+    
+    Returns:
+        search_mod (int): Возвращает пользовательский ответ 
+    """
+    print("\nВыберите вариант поиска:\n",
+          "1. Поиск по имени\n",
+          "2. Поиск по фамилии\n",
+          "3. Поиск по телефону\n",
+          "4. поиск по комментарию\n",
+          "5. отмеить\n")
+    search_mod = int(input())
+    return search_mod
+
+
+def load_phone_book(phone_book_file):
+    """
+    Функция загружает данные из файла JSON
+
+    Args:
+        phone_book_file (str): строка с названием файла
+
+    Returns:
+        phone_book (list): Спискок словарей контактов
+
+    """
+    with open(phone_book_file, "r") as f:
+        return json.load(f)
+
+
+def save_phone_book(phone_book, phone_book_file = "phonebook.json"):
+    """
+    Функция сохраняет список контактов в файл JSON
+
+    Args: 
+        phone_book (list): список контактов
+        phone_book_file (str) = "phonebook.json": строка с названием файла (значение по умолчанию)
+    """
+    with open(phone_book_file, "w", encoding="utf-8") as f:
+        json.dump(phone_book, f, indent=4, ensure_ascii=False)
+
+    print("="*len("Данные записной книги обновлены"))
+    print("Данные записной книги обновлены")
+    print("="*len("Данные записной книги обновлены"))
+
+def show_contacts(contacts):
+    """
+    Функция отображения контактов. 
+    Для корректного отображения опеределяет длину максимального эддемента в строке.
+    
+    Args:
+        contacts (list): Список словарей контактов
+    """
+
+    if contacts == None:
+        os.system('cls||clear')
+        print("По данному запросу нет контактов для отображения")
+        return
+    # Вычисление ширины столбцов
+    max_name_len = max(len(contact["name"]) for contact in contacts)
+    max_surname_len = max(len(contact["surname"]) for contact in contacts)
+    max_phone_len = max(len(contact["phone"]) for contact in contacts)
+    max_description_len = max(len(contact["description"]) for contact in contacts)
+
+    # Вывод таблицы в консольал
+    print("| {:<{}} | {:<{}} | {:<{}} | {:<{}} |".format("Имя", max_name_len, "Фамилия", max_surname_len, "Телефон", max_phone_len, "Описание", max_description_len))
+    print("-" * (max_name_len + max_surname_len + max_phone_len + max_description_len + 13))
+    for contact in contacts:
+        print("| {:<{}} | {:<{}} | {:<{}} | {:<{}} |".format(
+            contact["name"], max_name_len, contact["surname"], max_surname_len, contact["phone"], max_phone_len, contact["description"], max_description_len
+        ))
+
+
+def find_contact(contacts):
+    """ 
+    Функция для поиска абонента в телефонной книге
+
+    Args:
+        contacts (list): Список контактов
+    Returns:
+        contact (list): Список словарей с данными контактов
+        None: Если контакт не найден
+    """
+    if contacts == None:
+        print("Вы ввели некорректные данные, попробуйте еще раз")
+    search_mod = show_search_menu()
+    search_str = None
+    data = []
+    while True:
+        if search_mod == 1:
+            search_mod = "name"
+            print("Введите имя для поиска")
+            search_str = input()
+            break
+        elif search_mod==2:
+            search_mod = "surname"
+            print("Введите фамилию для поиска")
+            search_str = input()
+            break
+        elif search_mod==3:
+            search_mod = "phone"
+            print("Введите телефон для поиска")
+            search_str = input()
+            break
+        elif search_mod==4:
+            search_mod = "description"
+            print("Введите описание для поиска")
+            search_str = input()
+            break
+        elif search_mod==5:
+            return None
+        search_mod=show_search_menu()
+            
+    for contact in contacts:
+        if search_str.lower() in contact[search_mod].lower():
+            data.append(contact)
+    if len(data) > 0:
+        return data
+    return None
+
+
+def add_contact(contacts):
+    """
+    Функция добавления нового контакта
+
+    Args:
+        contacts (list): Список контактов
+    Returns:
+        contacts (list): Список контактов с добавленным в конец новым контактом
+    """
+    name = input("Введите имя: ")
+    surname = input("Введите фамилию: ")
+    phone = input("Введите номер телефона: ")
+    description = input("Введите описание: ")
+    new_contact = {"name": name, "surname": surname, "phone": phone, "description": description}
+    contacts.append(new_contact)
+    return contacts
+
+
+def remove_contact(contacts, contact_to_del):
+    """
+    Функция удаления контакта
+
+    Args:
+        contacts (list): Список контактов
+        contact_to_del (list): Контакт для удаления
+    Returns:
+        contacts (list): Список контактов без удаленного контакта
+    """
+    remove_index = None
+    if contact_to_del == None or len(contact_to_del) == 0:
+        print("Такого контакта не найдено")
+        return
+    elif len(contact_to_del) == 1:
+        remove_index = 0
+    else:
+        show_contacts(contact_to_del)
+        print("Введите порядковый номер контакта для удаления")
+        remove_index = int(input()) - 1
+    contacts.remove(contact_to_del[remove_index])
+    return contacts
+
+
+def edit_contact(contacts):
+    """
+    Функция редактирует контакт и возвращает обновленный список контактов
+
+    Args:
+        contacts (list): Список контактов
+    Returns:
+        contacts (list): Список контактов с обновленными данными
+    """
+    contact = find_contact(contacts)
+    choice = None
+    if contact is None:
+        print("Контакт не найден.")
+        return
+    elif len(contact) > 1:
+        print("Введите номер контакта для изменения")
+        show_contacts(contact)
+        choice = int(input()) - 1
+    if choice != None and choice + 1 <= len(contact):
+        contact = contact[choice]
+    else:
+        contact = contact[0]
+
+    print("Введите новые данные:")
+    name = input("Имя: ") or contact["name"]
+    surname = input("Фамилия: ") or contact["surname"]
+    phone = input("Номер телефона: ") or contact["phone"]
+    description = input("Описание: ") or contact["description"]
+
+    contact["name"] = name
+    contact["surname"] = surname
+    contact["phone"] = phone
+    contact["description"] = description
+    return contacts
+
+
+# Функция для сохранения справочника в текстовый файл
+def save_contacts_to_txt(contacts, file_name = "phone_book.txt"):
+    """
+    Функция для сохранения контактов в файл txt
+
+    Args:
+        contacts (list)
+        file_name  = "phone_book.txt" (str): будущее название файла !!! разрешение .txt
+    """
+    if contacts == None:
+        os.system('cls||clear')
+        print("По данному запросу нет контактов для отображения")
+        return
+    # Вычисление ширины столбцов
+    max_name_len = max(len(contact["name"]) for contact in contacts)
+    max_surname_len = max(len(contact["surname"]) for contact in contacts)
+    max_phone_len = max(len(contact["phone"]) for contact in contacts)
+    max_description_len = max(len(contact["description"]) for contact in contacts)
+
+    # Вывод таблицы в консоль
+    with open(file_name, "w") as f:
+        f.write("| {:<{}} | {:<{}} | {:<{}} | {:<{}} |\n".format("Имя", max_name_len, "Фамилия", max_surname_len, "Телефон", max_phone_len, "Описание", max_description_len))
+        f.write("-" * (max_name_len + max_surname_len + max_phone_len + max_description_len + 13) + "\n")
+        for contact in contacts:
+            f.write("| {:<{}} | {:<{}} | {:<{}} | {:<{}} |\n".format(
+                contact["name"], max_name_len, contact["surname"], max_surname_len, contact["phone"], max_phone_len, contact["description"], max_description_len
+            ))
+
+
+def work_with_phonebook(phone_book_file):
+    """
+    Функция работы с записной книгой
+    Запускается при старте программы и выводит в консоль меню, благодаря которому происходит управление файлом записной книги
+
+    Args:
+        phone_book_file (str): назание файла записной книги JSON
+    """
+    # Показываем меню и получаем ответ пользователя.
+    choice = show_menu()
+    #  Читаем телефонный справочник из файла
+    while (choice!=7):
+        phone_book = load_phone_book(phone_book_file)
+        if choice==1: # Отобразить весь список
+            os.system('cls||clear')
+            show_contacts(phone_book)
+        elif choice==2: # Найти абонента
+            os.system('cls||clear')
+            show_contacts(find_contact(phone_book))
+        elif choice==3: # Добавить абонента в справочник
+            os.system('cls||clear')
+            add_contact(phone_book)
+            save_phone_book(phone_book, phone_book_file)
+        elif choice==4: # Изменить абонента
+            os.system('cls||clear')
+            edit_contact(phone_book)
+            save_phone_book(phone_book, phone_book_file)
+        elif choice==5: # Удалить абонента
+            os.system('cls||clear')
+            remove_contact(phone_book, find_contact(phone_book))
+            save_phone_book(phone_book, phone_book_file)
+        elif choice==6: # Сохранить справочник в текстовом формате
+            os.system('cls||clear')
+            save_contacts_to_txt(phone_book)
+        choice = show_menu()
+    os.system('cls||clear')
+    print("Вы закончили работу с записной книгой")
+work_with_phonebook(phone_book_file)
